80d017d89d7d5cca726ed361b4741372d8e5fe64
Walter M <waltermccone88@gmail.com>
2021-12-16 16:56:34 -0600

    Add create and destroy methods for the session controller to create login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,23 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Logged in successfully"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with your login details"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to login_path
+
+  end
+
 end
