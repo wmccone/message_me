22c92d6c8ac067be8850179ab2edc7ca54fe06a9
Walter M <waltermccone88@gmail.com>
2021-12-17 14:04:39 -0600

    Add notification messages to the application layer and create the partial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,4 +16,8 @@ import '@doabit/semantic-ui-sass'
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
+  
