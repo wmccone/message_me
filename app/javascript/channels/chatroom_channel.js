71bf39aed4a9e267c466dde4b088741dab1b3ecf
Walter M <waltermccone88@gmail.com>
2021-12-17 16:39:47 -0600

    Wire up the message partial to the message controller and broadcast messages to the chatroom channel

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,5 +11,6 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    $('#message-container').append(data.mod_message)
   }
 });
