7ac9142c2011c59eb4cc62483fd220d2639dac17
Walter M <waltermccone88@gmail.com>
2021-12-17 17:26:29 -0600

    Add some test code for auto scrolling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,17 +7,24 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import '@doabit/semantic-ui-sass'
+// scroll_bottom = function() {
+//   if ($('#messages').length > 0) {
+//     $('messages').scrollTop($('#messages')[0].scrollHeight)
+//   }
+// }
+
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  // scroll_bottom();
 })
-  
+
